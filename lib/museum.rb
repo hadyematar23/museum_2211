927901a7ea271dff74a05ecd0b0e0c7e48fe4de0
Hady Matar <hadyematar@gmail.com>
2023-01-03 09:50:24 -0700

    Completion of iteration 2

--- lib/museum.rb
+++ lib/museum.rb
@@ -11,5 +11,16 @@ class Museum
     @exhibits << exhibit
   end
 
-  
+  def recommend_exhibits(patron)
+    reccomendations = []
+    @exhibits.each do |exhibit|
+      patron.interests.each do |interest|
+        if exhibit.name == interest
+          reccomendations << exhibit
+        end
+      end
+    end 
+    return reccomendations
+    
+  end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -40,12 +40,15 @@ RSpec.describe Museum do
   end 
 
   it "can reccomend exhibits to the patrons" do 
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
     patron_2.add_interest("IMAX")
 
-    expect(dmns.reccomend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
-    expect(dmns.recommend_exhibits(patron_2)).to eq(imax)
+    expect(dmns.recommend_exhibits(patron_1)).to include(dead_sea_scrolls, gems_and_minerals)
+    expect(dmns.recommend_exhibits(patron_2)).to include(imax)
   end 
   end
 end 
